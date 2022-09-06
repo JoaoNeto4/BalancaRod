eb6ec8ea9a80254ef63da2a792e73e600cd9f257
JoaoNeto4 <joaoquatlo@hotmail.com>
2022-09-06 17:24:41 -0300

    second commit

---
+++ src/br/com/dao/OperadorDao.java
@@ -0,0 +1,111 @@
+
+package br.com.dao;
+
+import br.com.bean.Operador;
+import br.com.bean.Permissao;
+import br.com.conexao.Conexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+
+public class OperadorDao {
+    
+    public static void inserir(Operador op)throws SQLException{
+        Connection con = Conexao.getConexao();
+        String sql = "insert into TB_Operador( ID_Permissao, nome, senha, funcao, observacoes) VALUES(?,?,?,?,?)";
+        PreparedStatement stmt = con.prepareStatement(sql);
+        stmt.setInt(1, op.getPermissao().getId());
+        stmt.setString(2, op.getNome());
+        stmt.setString(3, op.getSenha());
+        stmt.setString(4, op.getFuncao());
+        stmt.setString(2, op.getObservacao());
+        stmt.execute();
+        JOptionPane.showMessageDialog(null, "Operador Salvo Com Sucesso!");
+        stmt.close();
+        con.close();
+    }
+    
+    public static void excluir(Operador op)throws SQLException{
+        Connection con = Conexao.getConexao();
+        String sql = "DELETE FROM TB_Operador where id=?";
+        PreparedStatement stmt = con.prepareStatement(sql);
+        stmt.setInt(1, op.getId());
+        stmt.execute();
+        JOptionPane.showMessageDialog(null, "Operador Excluido Com Sucesso!");
+        stmt.close();
+        con.close();
+    }
+    
+    public static void alterar(Operador op)throws SQLException{
+        Connection con = Conexao.getConexao();
+        String sql = "UPDATE TB_Operador SET ID_Permissao=?, nome=?, senha=?, funcao=?, observacoes=? WHERE id= ?";
+        PreparedStatement stmt = con.prepareStatement(sql);
+        stmt.setInt(1, op.getPermissao().getId());
+        stmt.setString(2, op.getNome());
+        stmt.setString(3, op.getSenha());
+        stmt.setString(4, op.getFuncao());
+        stmt.setString(5, op.getObservacao());
+        stmt.setInt(6, op.getId());
+        stmt.execute();
+        JOptionPane.showMessageDialog(null, "Operador Alterada Com Sucesso!");
+        stmt.close();
+        con.close();
+    }
+    
+    public static List<Operador> listar() throws SQLException {
+        List<Operador> listaControle = new ArrayList<>();
+        Connection con = Conexao.getConexao();
+        String sql = "select * from TB_Operador order by nome";
+        PreparedStatement stmt = con.prepareStatement(sql);
+        ResultSet rs = stmt.executeQuery();
+        while (rs.next()) {
+            Operador op = new Operador();
+            op.setId(rs.getInt("ID"));
+            stmt.setInt(1, op.getPermissao().getId());
+            stmt.setString(2, op.getNome());
+            stmt.setString(3, op.getSenha());
+            stmt.setString(4, op.getFuncao());
+            stmt.setString(5, op.getObservacao());
+            listaControle.add(op);
+        }
+        stmt.close();
+        rs.close();
+        con.close();
+        return listaControle;
+     }
+     
+     public static List<Operador> pesquisar(Operador op) throws SQLException {
+        List<Operador> listaOperador = new ArrayList<>();
+        Connection con = Conexao.getConexao();
+        String sql = "select P.ID, P.nivel, P.observacao, OP.ID, OP.id, P.ativo from TB_Permissao as P inner join TB_Operador as OP on P.id_controle=OP.id where P.nome like'"+op.getNome()+"%' order by P.nome";
+        PreparedStatement stmt = con.prepareStatement(sql);
+        ResultSet rs = stmt.executeQuery();
+        while (rs.next()) {
+            
+            Permissao p = new Permissao();
+            p.setId(rs.getInt("P.ID"));
+            p.setNivel(rs.getString("P.nivel"));
+            p.setObservacao(rs.getString("P.observacao"));
+            
+            Operador operador = new Operador();
+            operador.setId(rs.getInt("OP.ID"));
+            operador.setNome(rs.getString("OP.nome"));
+            operador.setSenha(rs.getString("OP.senha"));
+            operador.setFuncao(rs.getString("OP.funcao"));
+            operador.setObservacao(rs.getString("OP.observacoes"));
+            operador.setPermissao(p);
+            listaOperador.add(operador);
+            
+        }
+        stmt.close();
+        rs.close();
+        con.close();
+        return listaOperador;
+     }
+
+}
---
+++ src/br/com/dao/ProdutoDao.java
@@ -0,0 +1,97 @@
+
+package br.com.dao;
+
+import br.com.bean.Produtos;
+import br.com.conexao.Conexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+
+public class ProdutoDao {
+    
+    public static void inserir(Produtos prod)throws SQLException{
+        Connection con = Conexao.getConexao();
+        String sql = "insert into TB_Produto( produto, unidMed, observacao) VALUES(?,?,?)";
+        PreparedStatement stmt = con.prepareStatement(sql);
+        stmt.setString(1, prod.getProduto());
+        stmt.setString(2, prod.getUnidMed());
+        stmt.setString(3, prod.getObservacoes());
+        stmt.execute();
+        JOptionPane.showMessageDialog(null, "Produto Salvo Com Sucesso!");
+        stmt.close();
+        con.close();
+    }
+    
+    public static void excluir(Produtos prod)throws SQLException{
+        Connection con = Conexao.getConexao();
+        String sql = "DELETE FROM TB_Produto where id=?";
+        PreparedStatement stmt = con.prepareStatement(sql);
+        stmt.setInt(1, prod.getId());
+        stmt.execute();
+        JOptionPane.showMessageDialog(null, "Produto Excluido Com Sucesso!");
+        stmt.close();
+        con.close();
+    }
+    
+    public static void alterar(Produtos prod)throws SQLException{
+        Connection con = Conexao.getConexao();
+        String sql = "UPDATE TB_Produto SET produto=?, unidMed=?, observacao=? WHERE id= ?";
+        PreparedStatement stmt = con.prepareStatement(sql);
+        stmt.setString(1, prod.getProduto());
+        stmt.setString(2, prod.getUnidMed());
+        stmt.setString(3, prod.getObservacoes());
+        stmt.setInt(6, prod.getId());
+        stmt.execute();
+        JOptionPane.showMessageDialog(null, "Produto Alterado Com Sucesso!");
+        stmt.close();
+        con.close();
+    }
+
+    public static List<Produtos> listar() throws SQLException {
+        List<Produtos> listaProdutos = new ArrayList<>();
+        Connection con = Conexao.getConexao();
+        String sql = "select * from TB_Produto order by produto";
+        PreparedStatement stmt = con.prepareStatement(sql);
+        ResultSet rs = stmt.executeQuery();
+        while (rs.next()) {
+            Produtos prod = new Produtos();
+            prod.setId(rs.getInt("ID"));
+            stmt.setString(1, prod.getProduto());
+            stmt.setString(2, prod.getUnidMed());
+            stmt.setString(3, prod.getObservacoes());
+            listaProdutos.add(prod);
+        }
+        stmt.close();
+        rs.close();
+        con.close();
+        return listaProdutos;
+     }
+
+     public static List<Produtos> pesquisar(Produtos prod) throws SQLException {
+        List<Produtos> listaOperador = new ArrayList<>();
+        Connection con = Conexao.getConexao();
+        String sql = "select * from TB_Produto where produto like'"+prod.getProduto()+"%' order by produto";
+        PreparedStatement stmt = con.prepareStatement(sql);
+        ResultSet rs = stmt.executeQuery();
+        while (rs.next()) {
+    
+            Produtos produto = new Produtos();
+            produto.setId(rs.getInt("ID"));
+            produto.setProduto(rs.getString("produto"));
+            produto.setUnidMed(rs.getString("unidMed"));
+            produto.setObservacoes(rs.getString("observacao"));
+            listaOperador.add(produto);
+            
+        }
+        stmt.close();
+        rs.close();
+        con.close();
+        return listaOperador;
+     }
+
+}
