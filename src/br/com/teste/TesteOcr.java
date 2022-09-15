ad55a5d6d37181af23f57c4487c07f349ab6d70a
JoaoNeto4 <joaoquatlo@hotmail.com>
2022-09-15 14:44:39 -0300

    first commit

---
+++ src/br/com/teste/TesteOcr.java
@@ -0,0 +1,35 @@
+
+package br.com.teste;
+
+import java.io.File;
+import java.io.IOException;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+/**
+ *
+ * @author joao
+ */
+public class TesteOcr {
+
+    public static void main(String[] args) throws IOException {
+    //sudo apt-get install tesseract-ocr-por
+    //sudo apt-get install tesseract-ocr
+    //
+    //lib Tess4j_5.4
+
+     File imageFile = new File("/home/joao/Downloads/TESTE/teste.png");
+     //File imageFile = new File("./src/main/resources/teste.png");
+     Tesseract tess4j = new Tesseract();
+     tess4j.setDatapath("/usr/share/tesseract-ocr/4.00/tessdata");
+     tess4j.setLanguage("por");
+     try {
+         String result = tess4j.doOCR(imageFile);
+         System.out.println(result);
+     } catch (TesseractException e) {
+         System.err.println(e.getMessage());
+     }
+    }  
+    
+    
+}
